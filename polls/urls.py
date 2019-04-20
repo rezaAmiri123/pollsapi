541a876b1b9dcb402333fe8a904545b1193b3997
rezaAmiri123 <reza.amiri.ac@gmail.com>
2019-04-20 20:42:21 +0430

    generic views has been used

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import polls_list, polls_detail, PollList, PollDetail
+from .views import PollList, PollDetail, ChoiceList, CreateVote
 
 app_name = 'polls'
 
 urlpatterns = [
-    # path("polls/", polls_list, name="polls_list"),
-    # path("polls/<int:pk>/", polls_detail, name="polls_detail"),
-
     path("polls/", PollList.as_view(), name='poll_list'),
     path("polls/<int:pk>/", PollDetail.as_view(), name='poll_detail'),
+    path("choices/", ChoiceList.as_view(), name='choice_list'),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,22 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
 from .models import Poll, Vote, Choice
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import PollSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializers
+from rest_framework import generics
 
 
-def polls_list(request):
-    MAX_OBJECTS = 20
-    polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
-    return JsonResponse(data=data)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {"results": {
-        "question": poll.question,
-        "created_by": poll.created_by.username,
-        "pub_date": poll.pub_date
-    }}
-    return JsonResponse(data=data)
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(instance=polls, many=True).data
-        return Response(data=data)
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(instance=poll).data
-        return Response(data=data)
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializers
