e7dea91c3149375479acb73ec393ca6a699fcd97
rezaAmiri123 <reza.amiri.ac@gmail.com>
2019-04-21 13:13:00 +0430

    query set define has been imporved

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path("polls/", PollList.as_view(), name='poll_list'),
     path("polls/<int:pk>/", PollDetail.as_view(), name='poll_detail'),
-    path("choices/", ChoiceList.as_view(), name='choice_list'),
-    path("vote/", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name='choice_list'),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from .models import Poll, Vote, Choice
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializers
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class PollList(generics.ListCreateAPIView):
@@ -14,9 +16,21 @@ class PollDetail(generics.RetrieveDestroyAPIView):
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
+        return queryset
+
     serializer_class = ChoiceSerializer
 
 
-class CreateVote(generics.CreateAPIView):
-    serializer_class = VoteSerializers
+class CreateVote(APIView):
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get('voted_by')
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializers(data=data)
+        if serializer.is_valid():
+            vote = serializer.save().data
+            return Response(data=vote, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
