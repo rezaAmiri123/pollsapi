a99d5158a25b1aa359f26cdab69704b17b72d279
rezaAmiri123 <reza.amiri.ac@gmail.com>
2019-04-22 13:31:36 +0430

    APIRequestFactory test has been added

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from polls import views
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = views.PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            username='test',
+            email='test@example.com',
+            password='test'
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.uri,
+                HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         f'Expected Response Code 200, '
+                         f'received {response.status_code} instead.')
