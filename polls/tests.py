5f74b5543f4678d3afebda17ec0b2f7ebbbc7269
rezaAmiri123 <reza.amiri.ac@gmail.com>
2019-04-22 14:38:24 +0430

    circleci automating test has been added

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,4 @@
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework.test import APIRequestFactory
 from polls import views
 from django.contrib.auth import get_user_model
@@ -13,6 +13,8 @@ class TestPoll(APITestCase):
         self.user = self.setup_user()
         self.token = Token.objects.create(user=self.user)
         self.token.save()
+        # API Client will adding
+        self.client = APIClient()
 
     @staticmethod
     def setup_user():
@@ -31,3 +33,21 @@ class TestPoll(APITestCase):
         self.assertEqual(response.status_code, 200,
                          f'Expected Response Code 200, '
                          f'received {response.status_code} instead.')
+
+    def test_list2(self):
+        self.client.login(username='test', password='test')
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 200,
+                         f'Expected Response Code 200, '
+                         f'received {response.status_code} instead.')
+
+    def test_create(self):
+        self.client.login(username='test', password='test')
+        params = {
+            'question': "How are you today?",
+            'created_by': 1
+        }
+        response = self.client.post(self.uri, data=params)
+        self.assertEqual(response.status_code, 201,
+                 'Expected Response Code 201, received {0} instead.'
+                 .format(response.status_code))
